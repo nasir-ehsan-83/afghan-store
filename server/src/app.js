4d8ee769421b4a41b6f20b93e14e9dab5c845b91
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-06-28 16:08:06 +0430

    refactor(cors): updare 'CORS' configuration

--- server/src/app.js
+++ server/src/app.js
@@ -10,10 +10,10 @@ import productRoute from "./routes/product.routes.js";
 const app = express();
 
 app.use(cors({
-    origin : [
-        "http://localhost:5173", 
-        "http://127.0.0.1:5173"
-    ]
+  origin: "http://localhost:5173", 
+  credentials: true,              
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma", "Expires"]  
 }));
 
 app.use(express.json());
