13b1739bf6dd7dfe783e979cbfbb1a1facef21b6
Nasir Ahmad Ehsan <nasirahmadehsan1383@gmail.com>
2026-07-20 11:25:49 +0430

    feat(middleware): use costum exception/error-handling in middlewares

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,41 +1,34 @@
 import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/async.handler.js";
 import { UserModel } from "../models/user.model.js";
+import { forbidden, unuathorized } from "../utils/error.js";
 
 export const getCurrentUser = asyncHandler(async (req, res, next) => {
-    // get authorization token from req.headers
+
     const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    // if authorization-token is not provided or the token type is not Bearer
     if (!authHeader?.startsWith("Bearer ")) {
-        const error = new Error("Unauthorized: No token provided");
-        error.statusCode = 401;
-        throw error;
+        return unuathorized("No token provided");
     }
 
-    // make authHeader an array and get its second element 
     const token = authHeader.split(' ')[1];
 
     try {
-        // decode jwt-token and get user's details
-        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
-        
-        // check if user exist
+        const decoded = jwt.verify(
+            token,
+            process.env.ACCESS_TOKEN_SECRET_KEY
+        );
+
         const userExists = await UserModel.findById(decoded.id);
-        
-        // if user does not exist
+
         if (!userExists) {
-            const error = new Error("Unauthorized: User account no longer exists");
-            error.statusCode = 401;
-            throw error;
+            return unuathorized("User account no longer exists");
         }
 
         req.user = decoded;
         return next();
 
     } catch (err) {
-        const error = new Error("Forbidden: Invalid or expired token");
-        error.statusCode = 403;
-        throw error;
+        return forbidden("Forbidden: Invalid or expired token");
     }
 });
--- server/src/middlewares/role.check.middleware.js
+++ server/src/middlewares/role.check.middleware.js
@@ -1,28 +1,23 @@
 import { asyncHandler } from "../utils/async.handler.js";
+import { forbidden, unauthorized } from "../utils/error.js";
 
 export const checkRole = (allowedRoles) => {
     return asyncHandler(async (req, res, next) => {
-        // if user or user.role is not provided 
+
         if (!req.user || !req.user.role) {
-            return res.status(401).json({
-                status: "error",
-                message: "Unauthorized: No user or role found on request object."
-            });
+            return unauthorized("No user or role found on request object.");
         }
 
-        // make user.role as valid string
         const userRoleStr = String(req.user.role).toUpperCase().trim();
-        // make allowedRoles;s elements as valid string
-        const cleanAllowedRoles = allowedRoles.map(role => String(role).toUpperCase().trim());
 
-        // if allowedRoles does not include user.role 
+        const cleanAllowedRoles = allowedRoles.map((role) => {
+            String(role).toUpperCase().trim()
+        });
+
         if (!cleanAllowedRoles.includes(userRoleStr)) {
-            return res.status(403).json({
-                status: "error",
-                message: `Forbidden: Your role (${userRoleStr}) is not allowed.`
-            });
+            return forbidden(`Forbidden: Your role (${userRoleStr}) is not allowed.`);
         }
-        
+
         next();
     });
 };
